bca5c53f1c0c36cea350b80dfc9798651efca0ad
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-02-09 15:36:18 +0200

    create needed schema

---
+++ schemas/comment.ts
@@ -0,0 +1,17 @@
+export default {
+	name: 'comment',
+	type: 'document',
+	title: 'Comment',
+	fields: [
+		{
+			name: 'postedBy',
+			title: 'PostedBy',
+			type: 'postedBy',
+		},
+		{
+			name: 'comment',
+			title: 'Comment',
+			type: 'postedBy',
+		},
+	]
+}
