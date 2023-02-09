167b5102fc60d8db2b2a24f0f4888071d7e8f5c4
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-02-09 15:36:36 +0200

    add schemas to project

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user'
+import comment from './comment'
+import pin from './pin'
+import save from './save'
+import postBy from './postBy'
+
+export const schemaTypes = [user, comment, pin, save, postBy]
